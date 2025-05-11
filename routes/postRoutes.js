4a547c4072ff9c8b21d3967ee6985f4a1f6c4814
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2025-05-11 09:43:25 +0530

    integrationg the cloudinary for storing media files

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,3 +1,26 @@
+// const express = require("express");
+// const multer = require("multer");
+// const {
+//   createPost,
+//   getPosts,
+//   addReaction,
+//   addComment,
+//   deletePost,
+// } = require("../controllers/postController");
+// const { auth } = require("../middleware/authMiddleware");
+
+// const upload = multer({ dest: "uploads/" });
+
+// const router = express.Router();
+
+// router.get("/", getPosts);
+// router.post("/", [auth, upload.single("media")], createPost);
+// router.post("/:postId/reaction", auth, addReaction);
+// router.post("/:postId/comment", auth, addComment);
+// router.delete("/:postId", auth, deletePost);
+
+// module.exports = router;
+
 const express = require("express");
 const multer = require("multer");
 const {
